403aa55428f586b45a2f9be6a9b713ae2c843a70
Derek <derekisinternet@gmail.com>
2019-01-29 22:16:16 -0800

    first commit, broken state

--- digital_rain.sh
+++ digital_rain.sh
@@ -12,11 +12,13 @@ raindrop_indexes=() # keeps track of the state of each column
 chars=($(cat characters)) # list of symbols to display
 
 # COLORS
-reset=\u001b[0m #resets color to normal
-red="\033[0;31m"
-blue="\033[0;34m"
-green="\033[0;32m"
+RESET="\e[22m" #resets color to normal
+red="\e[0;31m"
+blue="\e[0;34m"
+green="\e[0;32m"
 bold="\e[1m"
+dim="\e[2m"
+bright="\e[1m"
 # WINDOW SIZE
 width=$(tput cols)
 height=$(tput lines)
@@ -28,25 +30,32 @@ height=$(tput lines)
 
 # returns a random character
 generate_char() {
-  i=$((RANDOM%${#chars[@]}))
-  echo "$green${chars[$i]}"
+  echo $((RANDOM%${#chars[@]}))
 }
 
 # draw a character at a specified location
 # $1 - row
 # $2 - column
+# $3 - optional color code to attach to output
+# $4 - optional intensity. Defaults to normal
 draw_char() {
   r=$1
   c=$2
+  color=$3
+  intensity=$4
   if [ -z $r ]; then r=0; fi
   if [ -z $c ]; then c=0; fi
+  if [[ "$4" != "bold" && "$intensity" != "dim" ]]; then 
+    intensity=$(tput sgr0)
+  fi
+  if [ -z $color ]; then color=$(tput setaf 2); fi
+
   tput cup $r $c
-  tput bold
   char=$(generate_char)
-  printf $char
+  printf "$intensity$color$char$RESET"
 }
 
-# find a column that is not running and starts it
+# finds a column that is not running and starts it
 start_drip() {
   i=$((RANDOM%width))
   # find a random column with a zero
@@ -54,32 +63,57 @@ start_drip() {
     i=$((RANDOM%width))
   done
   # draw and iterate
-  draw_char 0 $i
+  draw_char 0 $i $green $bold
   raindrop_indexes[$i]=1
 }
 
 # iterate through all the columns that have drops and iterate
 iterate_drops(){
+  # # starting work on iterating drops in random columns
+  # drops=${raindrop_indexes[*]}
+  # while [[ ${#drops} -gt 0 ]]; do
+  #   length=$((${#drips}-1))
+
+  # done
+
   for i in $( seq 0 $((width-1)) ); do
     curr_row="${raindrop_indexes[$i]}"
     if [[ $curr_row -gt 0 ]]; then
-      draw_char $curr_row $i
-      fade $curr_row $i
-      new_row=$(($curr_row+1))
-      raindrop_indexes[$i]=$new_row
+      if [[ $curr_row -eq $(($height-1)) ]]; then
+        raindrop_indexes[$i]=0
+      else
+        draw_char $curr_row $i
+        fade $curr_row $i
+        new_row=$(($curr_row+1))
+        raindrop_indexes[$i]=$new_row
+      fi
     fi
   done
 }
 
-# make characters in
+# make characters behind lead character dimmer
+# $1 - row
+# $2 - column
+# $3 - length of the fade
 fade() {
   row=$1
   col=$2
+  length=$3
+
+  tput cup $row $column
+
+  while [[ $length -ne 0 ]]; do
+    length=$((length-1))
+    row=$((row-1))
+    tput cup $row $column
+  done
+
 }
 
 # clean up the terminal on exit
 key_trap() {
   tput cup $height 0
+  tput sgr0
   exit 0
 }
 
@@ -97,13 +131,11 @@ init(){
 # the main loop of the script.
 main_loop() {
   clear # diff btw clear and tput clear?
-  # x=$(generate_string $rain_length $char_count)
-  # draw $x $(($RANDOM%$width))
   while : 
   do
     start_drip
     iterate_drops
-    sleep 0.2
+    # sleep 0.2
   done
 }
 
@@ -114,6 +146,6 @@ main_loop() {
 init
 main_loop
 
-# put cursor back in a convenient place
+# put cursor back in a convenient state and location
 tput sgr0
 tput cup $height 0
--- scratchpad.sh
+++ scratchpad.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
-reset=\u001b[0m #resets color to normal
-red="\033[0;31m"
-blue="\033[0;34m"
-green="\033[0;32m"
-bold="\e[1m"
 
-clear
-tput civis # hide cursor
-tput cup 0 0
-tput bold
-printf "hi"
-sleep 1
-tput sgr0
-tput cup 0 0
-printf "$bluehi"
-sleep 1
-echo
-tput sgr0
+
+# for i in {0..255} ; do
+#   printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
+#   if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
+#      printf "\n";
+#   fi
+# done
+
+x=$1
+
+if [[ "x" -ne "bold" ]]; then 
+  echo "no"
+fi
